05b0f17de6c7a4adc9c5900972251d99baa41b82
Jonathan Davies <jonathandavies89@yahoo.com>
2017-05-18 07:34:53 +0200

    make it better

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,6 +1,7 @@
 var nodemailer = require('nodemailer');
 var recaptcha = require('express-recaptcha');
-recaptcha.init('6Ld74SEUAAAAAEk-ZDWZbVw6Bcvb8zQQxgT4Tyqr', '6Ld74SEUAAAAADgWL3K4DpWylpJgyqb3qGXcEvpu');
+ recaptcha.init('6Ld74SEUAAAAAEk-ZDWZbVw6Bcvb8zQQxgT4Tyqr', '6Ld74SEUAAAAADgWL3K4DpWylpJgyqb3qGXcEvpu');
+
 
 var transporter = nodemailer.createTransport({
   service: 'Mailgun',
@@ -22,47 +23,35 @@ exports.contactGet = function(req, res) {
 /**
  * POST /contact
  */
-exports.contactPost = function(req, res) {
- 
+ exports.contactPost = function(req, res) {
    recaptcha.verify(req, function(error){
-        if(!error){
-
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('email', 'Email cannot be blank').notEmpty();
-  req.assert('message', 'Message cannot be blank').notEmpty();
-  req.sanitize('email').normalizeEmail({ remove_dots: false });
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('error', errors);
-    return res.redirect('/contact');
-  }
-
-  var mailOptions = {
-    from: req.body.name + ' ' + '<'+ req.body.email + '>',
-    to: 'contact@isithelo.com',
-    subject: '✔ Contact Form | Heavy-lifting',
-    text: req.body.message
-  };
-
-  transporter.sendMail(mailOptions, function(err) {
-    req.flash('success', { msg: 'Thank you! Your feedback has been submitted.' });
-    res.redirect('/contact');
-  });
-
-        }
+    if(!error){
+      req.assert('name', 'Name cannot be blank').notEmpty();
+      req.assert('email', 'Email is not valid').isEmail();
+      req.assert('email', 'Email cannot be blank').notEmpty();
+      req.assert('message', 'Message cannot be blank').notEmpty();
+      req.sanitize('email').normalizeEmail({ remove_dots: false });
+      var errors = req.validationErrors();
+      if (errors) {
+        req.flash('error', errors);
+        return res.redirect('/contact');
+      }
+      var mailOptions = {
+        from: req.body.name + ' ' + '<'+ req.body.email + '>',
+        to: 'contact@isithelo.com',
+        subject: '✔ Contact Form | Heavy-lifting',
+        text: req.body.message
+      };
+      transporter.sendMail(mailOptions, function(err) {
+        req.flash('success', { msg: 'Thank you! Your feedback has been submitted.' });
+        res.redirect('/contact');
+      });
+    }
             //success code 
-        else{
-          req.flash('error', { msg: ' Heads up!  You may very well be a robot!' });
-          return res.redirect('/contact');
-        }
+            else{
+              req.flash('error', { msg: ' Heads up!  You may very well be a robot!' });
+              return res.redirect('/contact');
+            }
             //error code 
-    })
-
-
-
-
-
-};
+          })
+ };
--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@ var crypto = require('crypto');
 var nodemailer = require('nodemailer');
 var passport = require('passport');
 var User = require('../models/User');
+var recaptcha = require('express-recaptcha');
+recaptcha.init('6Ld74SEUAAAAAEk-ZDWZbVw6Bcvb8zQQxgT4Tyqr', '6Ld74SEUAAAAADgWL3K4DpWylpJgyqb3qGXcEvpu');
+
 
 /**
  * Login required middleware
@@ -77,37 +80,53 @@ exports.signupGet = function(req, res) {
 /**
  * POST /signup
  */
-exports.signupPost = function(req, res, next) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('email', 'Email cannot be blank').notEmpty();
-  req.assert('password', 'Password must be at least 4 characters long').len(4);
-  req.sanitize('email').normalizeEmail({ remove_dots: false });
+ exports.signupPost = function(req, res, next) {
+   recaptcha.verify(req, function(error){
+    if(!error){ 
+      req.assert('name', 'Name cannot be blank').notEmpty();
+      req.assert('email', 'Email is not valid').isEmail();
+      req.assert('email', 'Email cannot be blank').notEmpty();
+      req.assert('password', 'Password must be at least 4 characters long').len(4);
+      req.sanitize('email').normalizeEmail({ remove_dots: false });
+      var errors = req.validationErrors();
+      if (errors) {
+        req.flash('error', errors);
+        return res.redirect('/signup');
+      }
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (user) {
+          req.flash('error', { msg: 'The email address you have entered is already associated with another account.' });
+          return res.redirect('/signup');
+        }
 
-  var errors = req.validationErrors();
 
-  if (errors) {
-    req.flash('error', errors);
-    return res.redirect('/signup');
-  }
+      User.findOne({ name: req.body.name }, function(err, user) {
+        if (user) {
+          req.flash('error', { msg: 'The username you have entered is already associated with another account.' });
+          return res.redirect('/signup');
+        }
 
-  User.findOne({ email: req.body.email }, function(err, user) {
-    if (user) {
-      req.flash('error', { msg: 'The email address you have entered is already associated with another account.' });
+
+        user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+
+});
+
+        });
+        user.save(function(err) {
+          req.logIn(user, function(err) {
+            res.redirect('/');
+          });
+        });
+      });
+    } else { 
+      req.flash('error', { msg: 'Heads up , You may very well be a robot.' });
       return res.redirect('/signup');
     }
-    user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    user.save(function(err) {
-      req.logIn(user, function(err) {
-        res.redirect('/');
-      });
-    });
-  });
-};
+  })
+ };
 
 /**
  * GET /account
